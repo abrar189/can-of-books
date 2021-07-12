373b9683b12d4449faf78cd404682161d11ca66c
abrar189 <algourabrar@gmail.com>
2021-07-12 14:32:30 +0300

    done

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ class App extends React.Component {
             <Switch>
               <Route exact path="/">
                 {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
-                {isAuthenticated == true?  <BestBooks />: <Login />}
+                {isAuthenticated === true?  <BestBooks />: <Login />}
                    {/* {/* {isAuthenticated && */}
                    <BestBooks />
                 
--- src/Header.js
+++ src/Header.js
@@ -20,7 +20,7 @@ class Header extends React.Component {
         <Link to="/">Home</Link>
         <Link to="/profile">Profile</Link>
         {/* TODO: if the user is logged in, render the `LogoutButton` - if the user is logged out, render the `LoginButton` */}
-        {isAuthenticated == true?  <LogoutButton />: <LoginButton />}
+        {isAuthenticated === true?  <LogoutButton />: <LoginButton />}
 
       </Navbar>
     );
